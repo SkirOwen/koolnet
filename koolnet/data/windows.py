aec1e2b5b117cc0208b74760d7bcceffe9cf3326
Owen <owen.allemang@gmail.com>
2023-05-26 12:33:50 +0100

    [WIP] ideas for the window extractor

---
+++ koolnet/data/windows.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+def get_window(koop_modes: np.ndarray, win_size: tuple) -> nd.array:
+	# pick a random points
+	# check if in bounds and the cylinder is not in
+	# create a padded area of padding == win_size, so guarantee to be in bounds.
+
+	pass
