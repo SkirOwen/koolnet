f0727dbf08505d951b898f29c1bdfa6f53da876f
Owen Allemang <owen.allemang@gmail.com>
2023-05-29 21:05:39 +0100

    added scaffolding for Dataset (with pl) and random forest

---
+++ koolnet/dataset/koolset.py
@@ -0,0 +1,12 @@
+import pytorch_lightning as pl
+
+
+class Koolset:
+	def __init__(self):
+		pass
+
+	def __len__(self):
+		pass
+
+	def __getitem__(self, idx):
+		pass
---
+++ koolnet/models/random_forest.py
@@ -0,0 +1,9 @@
+from sklearn.ensemble import RandomForestRegressor
+
+
+def run_rf():
+	pass
+
+
+if __name__ == "__main__":
+	run_rf()
