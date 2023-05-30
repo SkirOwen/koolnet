aaec6f811056432bd07253d1cb90fe272703caee
Owen <owen.allemang@gmail.com>
2023-05-30 22:24:01 +0100

    added TODOs

--- koolnet/data/windows.py
+++ koolnet/data/windows.py
@@ -5,11 +5,14 @@ def gen_window_coord(koop_modes_xy: tuple[int, int], win_size: tuple[int, int], 
 	# pick a random points
 	# check if in bounds and the cylinder is not in
 	# create a padded area of padding == win_size, so guarantee to be in bounds.
+	# TODO: set a seed for the random.randint
 
 	x, y = koop_modes_xy
 	obst_x, obst_y, obst_r = obst_pos
 
 	win_size_x, win_size_y = win_size
+
+	# TODO: check if win_size > size and size - obst, otherwise goes to default values
 
 	wx0 = np.random.randint(obst_x + (2 * obst_r), x - win_size_x)
 	wy0 = np.random.randint(obst_y + (2 * obst_r), y - win_size_y)
@@ -24,6 +27,7 @@ def get_data_window(koop_modes: np.ndarray, window_coords: tuple[int, int, int, 
 	wx0, wx1, wy0, wy1 = window_coords
 	# TODO: check the off by one for the slicing
 	# np.random.randint is [a, b) is semi-open
+	# TODO: change the slicing as array are y, x
 	return koop_modes[wx0:wx1, wy0:wy1]
 
 
