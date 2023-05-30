fe6632d6f7499b8627c07b514f50b2b22b8ef69b
Owen <owen.allemang@gmail.com>
2023-05-30 22:21:32 +0100

    function to load h5

--- koolnet/utils/file_ops.py
+++ koolnet/utils/file_ops.py
@@ -2,6 +2,12 @@ import numpy as np
 import h5py
 
 
-
 def load_npz(filename: str) -> dict:
 	return np.load(filename)
+
+
+def load_h5(filename: str) -> tuple:
+	with h5py.File(filename, 'r') as file:
+		loaded_data = file['data'][:]
+		loaded_metadata = dict(file.attrs.items())
+	return loaded_data, loaded_metadata
