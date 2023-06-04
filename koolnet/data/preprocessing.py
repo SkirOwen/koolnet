bab4e1dc478b57e4651e5db81bcd485741a6c0e9
Owen <owen.allemang@gmail.com>
2023-06-04 16:25:22 +0100

    code for distance window to obstacle, and fix the code to generate the data and keeping the same window for all the modes together

--- koolnet/data/preprocessing.py
+++ koolnet/data/preprocessing.py
@@ -3,7 +3,14 @@ import numpy as np
 from tqdm import tqdm
 
 from koolnet.utils.file_ops import load_h5
-from koolnet.data.windows import get_data_win_size,get_data_window, gen_window_coord
+from koolnet.data.windows import get_data_win_size, get_data_window, gen_window_coord
+
+
+def dist_win_obst(obst_xy: tuple[int, int], win_coords: tuple[int, int, int, int]) -> tuple[int, int]:
+	x0, y0, _, _ = win_coords
+	dx = x0 - obst_xy[0]
+	dy = y0 - obst_xy[1]
+	return dx, dy
 
 
 def get_koop_mode(data, mode: int = 1):
@@ -17,7 +24,7 @@ def get_koop_mode(data, mode: int = 1):
 def get_mode_data(mode: int):
 	# TODO: this is currently broken, as I need to save the Idd or the xi_
 	# TODO: and not only xi
-	data, metadata = load_h5("xi")
+	data, metadata = load_h5("xi.h5")
 	koopmode = get_koop_mode(data, mode)
 	obst_pos = metadata["obst_x"], metadata["obst_y"], metadata["obst_r"]
 	obst_xy = obst_pos[:2]
@@ -27,14 +34,20 @@ def get_mode_data(mode: int):
 	return data_window, obst_xy
 
 
-def get_allmode_data(for_rf: bool, win_per_mode: int):
+def get_allmode_data(for_rf: bool, win_per_mode: int, win_size: tuple):
 	# TODO: Careful this is for one file, of one simulation
 	# TODO: having a way to see the sampled windows
-	data, metadata = load_h5("xi")
+	data, metadata = load_h5("xi.h5")
 	allmodes = metadata["powers"]
+	xi = data.reshape(-1, 100, 400)
 
 	obst_pos = metadata["obst_x"], metadata["obst_y"], metadata["obst_r"]
 	obst_xy = obst_pos[:2]
+	# TODO: fix the position of the obstacle
+
+	x_data = []
+	y_data = []
+	win_data = []
 
 	for w in tqdm(range(win_per_mode), leave=False, desc="Wind #"):
 		koopmodes_xy = xi.shape[1::-1]
@@ -43,42 +56,21 @@ def get_allmode_data(for_rf: bool, win_per_mode: int):
 		win_data.append(window_coords)
 		wx0, wy0, _, _ = window_coords
 
-	for mode in tqdm(allmodes):
-		for w in range(win_per_mode):
+		# TODO: check this
+		dist_x, dist_y = dist_win_obst(obst_xy, window_coords)
+
+		y_data.append((dist_x, dist_y))
+		x_mode_r = []
+		x_mode_abs = []
+		for mode in tqdm(allmodes, leave=False, desc="Mode #"):
 			koopmode = get_koop_mode(data, mode)
-			koopmodes_xy = koopmode.shape[:2]
-			window_coords = gen_window_coord(koopmodes_xy, win_size=(10, 10),  obst_pos=obst_pos)
 			data_window = get_data_window(koopmode, window_coords)
-			wx0, _, wy0, _ = window_coords
-
-			dist_x = wx0 - metadata["obst_x"]
-			dist_y = wy0 - metadata["obst_y"]
-			y.append((dist_x, dist_y))
 			if for_rf:
-				wind_r, wind_c = np.real(data_window), np.imag(data_window)
-				x.append((wind_r.flatten().sum(), wind_c.flatten().sum()))
-			else:
-				x.append(data_window)
+				wind_r, wind_abs = np.real(data_window), np.abs(data_window)
+				x_mode_r.append(wind_r.flatten().sum())
+				x_mode_abs.append(wind_abs.flatten().sum())
+		if for_rf:
+			x_data.append((np.sum(x_mode_r), np.sum(x_mode_abs)))
 
-	return np.array(x), np.array(y)
+	return np.array(x_data), np.array(y_data), np.array(win_data)
 
-
-####
-# 100, real_only
-# rmse
-# Out[63]: 56.10645682862513
-# r2 = r2_score(y_test, y_pred)
-# r2
-# Out[65]: -0.005008137710641458
-#
-# 300, real_only
-# rmse
-# Out[73]: 61.07491857575466
-# r2
-# Out[74]: -0.009413718719932684
-#
-# 300, real and imag sum
-# rmse
-# Out[78]: 50.265797687028005
-# r2
-# Out[79]: 0.3665034130767686
