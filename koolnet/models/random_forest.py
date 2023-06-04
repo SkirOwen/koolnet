09f2906bcb7d5f2e9c8a933a4c50fa947275275a
Owen <owen.allemang@gmail.com>
2023-06-04 16:23:24 +0100

    added code for an IoU

--- koolnet/models/random_forest.py
+++ koolnet/models/random_forest.py
@@ -22,6 +22,32 @@ def test_rf(rf_model, X_test, y_test):
 	return rmse, r2
 
 
+def cal_iou(pred, obst_pos) -> float:
+	# assuming same radius
+	xp, yp, rp = pred
+	xo, yo, ro = obst_pos
+	area_p = np.pi * rp ** 2
+	area_o = np.pi * ro ** 2
+
+	d = np.sqrt((xp - xo)**2 + (yp - yo)**2)
+
+	if d >= rp + ro:
+		iou = 0
+	elif d <= np.abs(rp - ro):
+		iou = 1
+	else:
+		ai = (
+			rp**2 * np.arccos((d**2 + rp**2 - ro**2) / (2 * d * rp)) +
+			ro**2 * np.arccos((d**2 + ro**2 - rp**2) / (2 * d * ro)) -
+			0.5 * np.sqrt((-d + rp + ro) * (d + rp - ro) * (d - rp + ro) * (d + rp + ro))
+		)
+		au = area_p + area_o - ai
+
+		iou = ai / au
+
+	return iou
+
+
 def bar(avg, win_per_mode):
 	from tqdm import tqdm
 	rmse_lst, r2_lst = [], []
