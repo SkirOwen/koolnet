9b686b6ff97046e072ab2d8580365773d77f97ea
Owen <owen.allemang@gmail.com>
2023-06-04 16:22:09 +0100

    plotting to plot a prediction and a window on top of the data

---
+++ koolnet/utils/plotting.py
@@ -0,0 +1,91 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from koolnet.utils.file_ops import load_h5
+from koolnet.data.windows import gen_window_coord
+
+
+def plot_window(data, win_coords, obst_pos, pred):
+	wx0, wy0, wx1, wy1 = win_coords
+
+	plot_x, plot_y = data.shape[:2]
+
+	# Wind coord plot space
+	wx0ps, wx1ps, wy0ps, wy1ps = wx0, wx1, wy0, wy1
+
+	x, y = np.meshgrid(np.arange(plot_x), np.arange(plot_y))
+	obst_x, obst_y, obst_r = obst_pos
+
+	d = 2 * obst_r
+	d = 1
+	fig, axs = plt.subplots(2, 1)
+
+	contour_1 = np.linspace(np.min(np.real(data)), np.max(np.real(data)), 21)
+
+	c1 = axs[0].contourf(
+		# (x - obst_x) / d,
+		# (y - obst_y) / d,
+		x,
+		y,
+		np.real(data.T),
+		contour_1,
+		# cmap=cm.cm.curl,
+		# vmin=np.min(contourp_1),
+		# vmax=np.max(contourp_1)
+	)
+	# cbar1 = fig.colorbar(c1, ax=axs[0])
+	# cbar1.formatter.set_powerlimits((-2, 2))  # Display colorbar tick labels in scientific notation
+	# cbar1.update_ticks()
+
+	axs[0].fill(
+		obst_r * np.cos(np.arange(0, 2 * np.pi, 0.01)) + obst_x,
+		obst_r * np.sin(np.arange(0, 2 * np.pi, 0.01)) + obst_y,
+		"r"
+	)
+	axs[0].fill(
+		(np.array([wx0, wx1, wx1, wx0])),
+		(np.array([wy0ps, wy0ps, wy1ps, wy1ps])),
+		color=(0.7, 0.2, 0.3, 0.65)
+	)
+
+	scale_pred_x = (wx0 + pred[0])
+	scale_pred_y = (wy0ps + pred[1])
+
+	axs[0].fill(
+		(obst_r * np.cos(np.arange(0, 2 * np.pi, 0.01))) + scale_pred_x,
+		(obst_r * np.sin(np.arange(0, 2 * np.pi, 0.01))) + scale_pred_y,
+		color=(0.45, 0.23, 0.75, 0.8)
+	)
+
+	axs[0].fill(
+		(np.array([wx0, wx0 + pred[0]])),
+		(np.array([wy0ps, wy0ps + pred[1]])),
+		color=(0.37, 0.23, 0.17, 0.75)
+	)
+
+	print(f"TL Corner: {wx0}, {wy0ps} ({wy0})")
+	print(f"Absolute pos: {scale_pred_x}, {scale_pred_y}")
+	print(f"Relative pos: {pred[0]}, {pred[1]}")
+
+	# axs[0].set_ylim(axs[0].get_ylim()[::-1])
+	axs[0].set_xticks(np.arange(0, 400, 50))
+	axs[0].set_yticks(np.arange(0, 100, 10))
+	axs[0].set_aspect('equal')
+	plt.tight_layout()
+	plt.show()
+
+
+def main():
+	mode = 6
+	data, metadata = load_h5("xi.h5")
+	xi = data[mode - 1]
+	xi = (-1j * xi.reshape(100, 400)).T
+	xy = xi.shape[:2]
+
+	obst_pos = metadata["obst_x"], metadata["obst_y"], metadata["obst_r"]
+	win_coord = gen_window_coord(koop_modes_xy=xy, win_size=(10, 10), obst_pos=obst_pos)
+	plot_window(xi, win_coord, obst_pos, [50, -50])
+
+
+if __name__ == "__main__":
+	main()
