97298a5a5b1e00ae699b7c5fc8cd50b62c13d8be
Owen <owen.allemang@gmail.com>
2023-06-13 00:07:51 +0100

    fixed plotting issues

--- koolnet/data/preprocessing.py
+++ koolnet/data/preprocessing.py
@@ -7,9 +7,10 @@ from koolnet.data.windows import get_data_win_size, get_data_window, gen_window_
 
 
 def dist_win_obst(obst_xy: tuple[int, int], win_coords: tuple[int, int, int, int]) -> tuple[int, int]:
+	# TODO: Check this
 	x0, y0, _, _ = win_coords
-	dx = x0 - obst_xy[0]
-	dy = y0 - obst_xy[1]
+	dx = obst_xy[0] - x0
+	dy = obst_xy[1] - y0
 	return dx, dy
 
 
@@ -24,8 +25,10 @@ def get_koop_mode(data, mode: int = 1):
 def get_mode_data(mode: int):
 	# TODO: this is currently broken, as I need to save the Idd or the xi_
 	# TODO: and not only xi
-	data, metadata = load_h5("xi.h5")
-	koopmode = get_koop_mode(data, mode)
+	data, metadata = load_h5("xi_v2.h5")
+	# koopmode = get_koop_mode(data, mode)
+	mode_idx = list(metadata["powers"]).index(mode)
+	koopmode = data[mode_idx]
 	obst_pos = metadata["obst_x"], metadata["obst_y"], metadata["obst_r"]
 	obst_xy = obst_pos[:2]
 
@@ -37,9 +40,8 @@ def get_mode_data(mode: int):
 def get_allmode_data(for_rf: bool, win_per_mode: int, win_size: tuple):
 	# TODO: Careful this is for one file, of one simulation
 	# TODO: having a way to see the sampled windows
-	data, metadata = load_h5("xi.h5")
+	data, metadata = load_h5("xi_v2.h5")
 	allmodes = metadata["powers"]
-	xi = data.reshape(-1, 100, 400)
 
 	obst_pos = metadata["obst_x"], metadata["obst_y"], metadata["obst_r"]
 	obst_xy = obst_pos[:2]
@@ -50,7 +52,7 @@ def get_allmode_data(for_rf: bool, win_per_mode: int, win_size: tuple):
 	win_data = []
 
 	for w in tqdm(range(win_per_mode), leave=False, desc="Wind #"):
-		koopmodes_xy = xi.shape[1::-1]
+		koopmodes_xy = data.shape[1::]
 		# TODO: data is not the correct shape
 		window_coords = gen_window_coord(koopmodes_xy, win_size=win_size,  obst_pos=obst_pos)
 		win_data.append(window_coords)
@@ -63,7 +65,11 @@ def get_allmode_data(for_rf: bool, win_per_mode: int, win_size: tuple):
 		x_mode_r = []
 		x_mode_abs = []
 		for mode in tqdm(allmodes, leave=False, desc="Mode #"):
-			koopmode = get_koop_mode(data, mode)
+			# koopmode = get_koop_mode(data, mode)
+			# TODO: fix this!
+			mode_idx = list(metadata["powers"]).index(mode)
+			koopmode = data[mode_idx]
+
 			data_window = get_data_window(koopmode, window_coords)
 			if for_rf:
 				wind_r, wind_abs = np.real(data_window), np.abs(data_window)
--- koolnet/utils/plotting.py
+++ koolnet/utils/plotting.py
@@ -10,15 +10,14 @@ def plot_window(data, win_coords, obst_pos, pred):
 
 	plot_x, plot_y = data.shape[:2]
 
-	# Wind coord plot space
-	wx0ps, wx1ps, wy0ps, wy1ps = wx0, wx1, wy0, wy1
-
+	# Need to flip axes as Xs are rows and Ys are cols
 	x, y = np.meshgrid(np.arange(plot_x), np.arange(plot_y))
 	obst_x, obst_y, obst_r = obst_pos
 
 	d = 2 * obst_r
 	d = 1
-	fig, axs = plt.subplots(2, 1)
+
+	fig, axs = plt.subplots(2, 1, figsize=(10, 10))
 
 	contour_1 = np.linspace(np.min(np.real(data)), np.max(np.real(data)), 21)
 
@@ -37,54 +36,68 @@ def plot_window(data, win_coords, obst_pos, pred):
 	# cbar1.formatter.set_powerlimits((-2, 2))  # Display colorbar tick labels in scientific notation
 	# cbar1.update_ticks()
 
+	# Obstacle
 	axs[0].fill(
 		obst_r * np.cos(np.arange(0, 2 * np.pi, 0.01)) + obst_x,
 		obst_r * np.sin(np.arange(0, 2 * np.pi, 0.01)) + obst_y,
 		"r"
 	)
+	# Window
 	axs[0].fill(
 		(np.array([wx0, wx1, wx1, wx0])),
-		(np.array([wy0ps, wy0ps, wy1ps, wy1ps])),
+		(np.array([wy0, wy0, wy1, wy1])),
 		color=(0.7, 0.2, 0.3, 0.65)
 	)
 
 	scale_pred_x = (wx0 + pred[0])
-	scale_pred_y = (wy0ps + pred[1])
+	scale_pred_y = (wy0 + pred[1])
 
+	# Predicted obstacle
 	axs[0].fill(
 		(obst_r * np.cos(np.arange(0, 2 * np.pi, 0.01))) + scale_pred_x,
 		(obst_r * np.sin(np.arange(0, 2 * np.pi, 0.01))) + scale_pred_y,
 		color=(0.45, 0.23, 0.75, 0.8)
 	)
 
+	# Line
 	axs[0].fill(
 		(np.array([wx0, wx0 + pred[0]])),
-		(np.array([wy0ps, wy0ps + pred[1]])),
+		(np.array([wy0, wy0 + pred[1]])),
 		color=(0.37, 0.23, 0.17, 0.75)
 	)
 
-	print(f"TL Corner: {wx0}, {wy0ps} ({wy0})")
+	print(f"TL Corner: {wx0}, {wy0} ({wy0})")
 	print(f"Absolute pos: {scale_pred_x}, {scale_pred_y}")
 	print(f"Relative pos: {pred[0]}, {pred[1]}")
 
 	# axs[0].set_ylim(axs[0].get_ylim()[::-1])
-	axs[0].set_xticks(np.arange(0, 400, 50))
+	axs[0].set_xticks(np.arange(0, 400, 25))
 	axs[0].set_yticks(np.arange(0, 100, 10))
 	axs[0].set_aspect('equal')
 	plt.tight_layout()
+	# plt.savefig("./output/plots/fig.svg")
 	plt.show()
 
 
 def main():
-	mode = 6
-	data, metadata = load_h5("xi.h5")
-	xi = data[mode - 1]
-	xi = (-1j * xi.reshape(100, 400)).T
+	data, metadata = load_h5("xi_v2.h5")
+	mode = 18
+	mode_idx = list(metadata["powers"]).index(mode)
+	xi = data[mode_idx]
+	# xi = (-1j * xi.reshape(100, 400)).T
 	xy = xi.shape[:2]
 
 	obst_pos = metadata["obst_x"], metadata["obst_y"], metadata["obst_r"]
+	# win_coord = [
+	# 	obst_pos[0] - obst_pos[2],
+	# 	obst_pos[1] - obst_pos[2],
+	# 	obst_pos[0] + obst_pos[2],
+	# 	obst_pos[1] + obst_pos[2],
+	# ]
+	# print(f"{win_coord = }")
+
 	win_coord = gen_window_coord(koop_modes_xy=xy, win_size=(10, 10), obst_pos=obst_pos)
-	plot_window(xi, win_coord, obst_pos, [50, -50])
+	plot_window(xi, win_coord, obst_pos, [-30, 22])
 
 
 if __name__ == "__main__":
