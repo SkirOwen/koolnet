0b2a1d47ea164d48ebd8ac94bac3266100cea602
Owen <owen.allemang@gmail.com>
2023-06-25 17:02:09 +0100

    [WIP] regressive AlexNet

--- koolnet/models/kool_net.py
+++ koolnet/models/kool_net.py
@@ -8,69 +8,63 @@ import lightning.pytorch as pl
 
 
 class KoolNet(nn.Module):
-	def __int__(self):
+	def __init__(self, mode_nbr, dropout: float):
 		super().__init__()
-		self.c1 = nn.Sequential(
-			nn.Conv2d(),
-			nn.Tanh(),
-			nn.MaxPool2d(),
+
+		self.mode_nbr = mode_nbr
+
+		self.features = nn.Sequential(
+			nn.Conv2d(self.mode_nbr, 64, kernel_size=11, stride=4, padding=2),
+			nn.Tanh(inplace=True),
+			nn.MaxPool2d(kernel_size=3, stride=2),
+			nn.Conv2d(64, 192, kernel_size=5, padding=2),
+			nn.Tanh(inplace=True),
+			nn.MaxPool2d(kernel_size=3, stride=2),
+			nn.Conv2d(192, 384, kernel_size=3, padding=1),
+			nn.Tanh(inplace=True),
+			nn.Conv2d(384, 256, kernel_size=3, padding=1),
+			nn.Tanh(inplace=True),
+			nn.Conv2d(256, 256, kernel_size=3, padding=1),
+			nn.Tanh(inplace=True),
+			nn.MaxPool2d(kernel_size=3, stride=2),
 		)
 
-		self.c2 = nn.Sequential(
-			nn.Conv2d(),
+		self.conv = nn.Sequential(
+			nn.Conv2d(self.mode_nbr, 96, kernel_size=(3, 3), stride=1),
 			nn.Tanh(),
-			nn.MaxPool2d(),
-		)
-		self.c3 = nn.Sequential(
-			nn.Conv2d(),
+			nn.MaxPool2d(kernel_size=3, stride=2),
+			nn.Conv2d(96, 256, kernel_size=(5, 5), stride=2),
 			nn.Tanh(),
-		)
-		self.c4 = nn.Sequential(
-			nn.Conv2d(),
+			nn.MaxPool2d(kernel_size=5),
+			nn.Conv2d(256, 384, kernel_size=(3, 3), stride=2),
 			nn.Tanh(),
-		)
-		self.c5 = nn.Sequential(
-			nn.Conv2d(),
+			nn.Conv2d(384, 384,	kernel_size=(3, 3), stride=4),
 			nn.Tanh(),
-			nn.MaxPool2d(),
+			nn.Conv2d(384, 384, kernel_size=(3, 3), stride=4),
+			nn.Tanh(),
+			nn.MaxPool2d(kernel_size=3),
 		)
 
-		self.fc1 = nn.Sequential(
-			nn.Linear(),
-			nn.Dropout(),
+		self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+
+		self.lin = nn.Sequential(
+			nn.Dropout(p=dropout),
+			nn.Linear(in_features=384, out_features=4096),
 			nn.Tanh(),
-		)
-
-		self.fc2 = nn.Sequential(
-			nn.Linear(),
-			nn.Dropout(),
+			nn.Linear(in_features=4096, out_features=4096),
 			nn.Tanh(),
-		)
-
-		self.fc3 = nn.Sequential(
-			nn.Linear(),
-			nn.Dropout(),
+			nn.Dropout(p=dropout),
+			nn.Linear(in_features=4096, out_features=4096),
 			nn.Tanh(),
-		)
-
-		self.out = nn.Sequential(
-			nn.Linear(in_features=4000, out_features=2),
-		)
-
-		self.model = nn .Sequential(
-			self.c1(),
-			self.c2(),
-			self.c3(),
-			self.c4(),
-			self.c5(),
-			self.fc1(),
-			self.fc2(),
-			self.fc3(),
-			self.fc4(),
+			nn.Dropout(p=dropout),
+			nn.Linear(in_features=4096, out_features=2),
 		)
 
 	def forward(self, x):
-		return self.model(x)
+		x = self.conv(x)
+		x = torch.flatten(x, 1)
+		x = self.lin(x)
+		return x
 
 
 def main():
