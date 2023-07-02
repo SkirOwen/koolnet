c628333be88c95eea7894901920380078a9de2cf
Owen <owen.allemang@gmail.com>
2023-07-02 15:29:15 +0100

    [WIP] Kool Dataset

--- koolnet/dataset/koolset.py
+++ koolnet/dataset/koolset.py
@@ -1,13 +1,27 @@
 import torch
 from torch.utils.data import Dataset
 
+from koolnet.data.preprocessing import get_allmode_data
+
 
 class Koolset(Dataset):
-	def __init__(self):
-		pass
+	def __init__(self, x, rel_pos, w):
+		self.x = x
+		self.rel_pos = rel_pos
+		self.w = w
 
 	def __len__(self):
-		pass
+		return len(self.x)
 
 	def __getitem__(self, idx):
-		pass
+		if torch.is_tensor(idx):
+			idx = idx.tolist()
+
+		image = torch.as_tensor(self.x[idx]).float()
+		rel_pos_obst = torch.as_tensor(self.rel_pos[idx]).float()
+		#
+
+		return image, rel_pos_obst
+
+	def get_window(self, idx):
+		return torch.as_tensor(self.w[idx])
